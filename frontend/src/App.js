ed1a168f656ba1d574722f252faf3cb4247bcde5
Carl Costa <32249778+totatmeister@users.noreply.github.com>
2019-02-21 13:12:17 -0800

    Switched the navbar
    It is now called by the app instead of just the quizcreation page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import QuizCreation from "./QuizCreation";
+import PageNavigation from "./PageNavigation";
 class App extends Component {
   render() {
 
     return (
       <div className="App">
-        <QuizCreation />
+        <PageNavigation/>
+        <QuizCreation/>
       </div>
     );
   }
