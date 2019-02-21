46141df299fbcb55cd55d2170fc564b3e9865bf0
Carl Costa <32249778+totatmeister@users.noreply.github.com>
2019-02-21 15:03:29 -0800

    Added question form
    Added the frontend of the create a question form

--- frontend/src/QuizCreation.js
+++ frontend/src/QuizCreation.js
@@ -1,12 +1,50 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Button, ButtonGroup } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 class QuizCreation extends Component {
   render() {
-    
+
     return (
-      <div>
-      </div>
+      <Form>
+        <FormGroup>
+          <legend>Question</legend>
+          <Input type="textarea" name="question" id="question" placeholder="Question" />
+        </FormGroup>
+        <FormGroup>
+          <FormText color="muted">
+            Write the quiz question above, and then select the correct answer by selecting the radiobutton on its side.
+        </FormText>
+        </FormGroup>
+        <FormGroup tag="answers">
+          <legend>Answers</legend>
+          <FormGroup check>
+            <Input type="radio" name="radio1" />{' '}
+            <Label check>
+              <Input type="textarea" name="text" id="optionOne" placeholder="Option One" />
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+
+            <Input type="radio" name="radio1" />{' '}
+            <Label check>
+              <Input type="textarea" name="text" id="optionTwo" placeholder="Option Two" />
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Input type="radio" name="radio1" />{' '}
+            <Label check>
+              <Input type="textarea" name="text" id="optionThree" placeholder="Option Three" />
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Input type="radio" name="radio1" />{' '}
+            <Label check>
+              <Input type="textarea" name="text" id="optionFour" placeholder="Option Four" />
+            </Label>
+          </FormGroup>
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
     );
   }
 }
