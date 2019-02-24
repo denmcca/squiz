913868378e745c818ccb1c06fd9daedf9909a32d
kirbubu <38482025+kirbubu@users.noreply.github.com>
2019-02-24 01:26:50 -0800

    Add Backend-Frontend Connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import QuizCreation from "./QuizCreation";
 import PageNavigation from "./Components/PageNavigation";
 import Router from "./Components/Router";
+import { Provider } from 'react-redux';
+import store from './store';
+
 class App extends Component {
   render() {
 
     return (
-      <div className="App">
-        <PageNavigation/>
-        <Router/>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <PageNavigation />
+          <Router />
+        </div>
+      </Provider>
     );
   }
 }
