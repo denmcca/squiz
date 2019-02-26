39c11f29eb0057ae3377a22df9a34d7cfbe8bcb5
Carl Costa <32249778+totatmeister@users.noreply.github.com>
2019-02-26 13:00:12 -0800

    Added Question List
    The list displays at the bottom, the values still needs to be linked with the form on the top
    
    Note: consider putting the form at the bottom

--- frontend/src/Pages/CreateQuizPage.jsx
+++ frontend/src/Pages/CreateQuizPage.jsx
@@ -2,13 +2,29 @@ import React, { Component } from "react";
 import "../App.css";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 class CreateQuizPage extends Component {
+  constructor() {
+    super();
+    // the state stores the list of questions 
+    this.state = {
+      questions: [{ question: "Question Name Here", optionOne: "", optionTwo: "", optionThree: "", optionFour: "" }],
+    }
+    // listeners
+    //this.handleInputChange = this.handleInputChange.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  // adding a question to the list
+  addQuestion = (e) => {
+    this.setState((prevState) => ({
+      // add a new set of question to the list
+      questions: [...prevState.questions, { question: "", optionOne: "", optionTwo: "", optionThree: "", optionFour: "" }],
+    }));
+  }
   render() {
-
     return (
       <Form>
-        <FormGroup style = {{marginLeft: '100px', marginRight: '100px'}}>
+        <FormGroup style={{ marginLeft: '100px', marginRight: '100px' }}>
           <legend>Question</legend>
-          <Input type="textarea" name="question" placeholder="Question"/>
+          <Input type="textarea" name="question" placeholder="Question" />
         </FormGroup>
         <FormGroup>
           <FormText color="muted">
@@ -20,30 +36,49 @@ class CreateQuizPage extends Component {
           <FormGroup check>
             <Input type="radio" name="radio1" />{' '}
             <Label check>
-              <Input type="textarea" name="optionOne" placeholder="Option One" style = {{width: "500px"}} />
+              <Input type="textarea" name="optionOne" placeholder="Option One" style={{ width: "500px" }} />
             </Label>
           </FormGroup>
           <FormGroup check>
 
             <Input type="radio" name="radio1" />{' '}
             <Label check>
-              <Input type="textarea" name="optionTwo" placeholder="Option Two" style = {{width: "500px"}}/>
+              <Input type="textarea" name="optionTwo" placeholder="Option Two" style={{ width: "500px" }} />
             </Label>
           </FormGroup>
           <FormGroup check>
             <Input type="radio" name="radio1" />{' '}
             <Label check>
-              <Input type="textarea" name ="optionThree" placeholder="Option Three" style = {{width: "500px"}}/>
+              <Input type="textarea" name="optionThree" placeholder="Option Three" style={{ width: "500px" }} />
             </Label>
           </FormGroup>
           <FormGroup check>
             <Input type="radio" name="radio1" />{' '}
             <Label check>
-              <Input type="textarea" name= "optionFour" placeholder="Option Four" style = {{width: "500px"}}/>
+              <Input type="textarea" name="optionFour" placeholder="Option Four" style={{ width: "500px" }} />
             </Label>
           </FormGroup>
         </FormGroup>
-        <Button>Submit</Button>
+        <Button onClick = {this.addQuestion}>Add New Question</Button>{
+          // Display the list of questions that have been added to the list
+          this.state.questions.map((val,idx) => {
+            let questionID = `question-${idx}`
+            return(
+              <div key = {idx}>
+                {/** Label of the question */}
+                <label htmlFor={questionID}>{`Question #${idx+1}`}</label>
+                <input
+                type = "text"
+                name = {questionID}
+                data-id = {idx}
+                id = {questionID}
+                value = {this.state.questions[idx].question}
+
+                />
+              </div>
+            )
+          })
+        }
       </Form>
     );
   }
