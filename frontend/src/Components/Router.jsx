88319077b93ba16569f6a2454f93066c7bd7f105
denmcca <denmcca@gmail.com>
2019-03-14 13:38:26 -0700

    fixed router bug

--- frontend/src/Components/Router.jsx
+++ frontend/src/Components/Router.jsx
@@ -4,18 +4,52 @@ import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
 import HomePage from '../Pages/HomePage';
 import CreateQuizPage from '../Pages/CreateQuizPage';
+import WelcomePage from '../Pages/Welcome';
+
 
 export default class Router extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    }
+  }
+
+  // isLoggedIn() {
+  //   // let message = this.state.isLoggedIn? 'Logged In' : 'Not Logged In';
+  //   let navPage = this.state.isLoggedIn? <PageNavigation /> : null;
+  //   let bodyPage = this.state.isLoggedIn? <CreateQuizPage /> : <Welcome />;
+  //   return (
+  //     <div>
+  //       {this.state.isLoggedIn? <PageNavigation /> : null}
+  //       {this.state.isLoggedIn? <CreateQuizPage /> : <Welcome />}
+  //     </div>
+  //   );
+  // }
+
   render() {
-    return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <div>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/login' component={LoginPage} />
-          <Route exact path='/CreateQuiz' component={CreateQuizPage} />
-          <Route exact path='/register' component ={RegisterPage} />
-        </div>
-      </BrowserRouter>
-    )
+    if(this.state.isLoggedIn){
+      return(
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <div>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/login' component={LoginPage} />
+            <Route exact path='/CreateQuiz' component={CreateQuizPage} />
+            <Route exact path='/register' component ={RegisterPage} />
+          </div>
+        </BrowserRouter>
+      )
+    }
+    else {
+      return (
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <div>
+            <Route component={WelcomePage} />
+            <Route exact path='/login' component={LoginPage} />
+          </div>
+        </BrowserRouter>
+      )
+    }
   }
 }
