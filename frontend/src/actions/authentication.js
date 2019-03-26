fe1ac105a5b128fcaa83dd37e7b285f18d706cd8
Philip Poppler <philpoppler@gmail.com>
2019-03-26 13:37:09 -0700

    Modify Authentication

--- frontend/src/actions/authentication.js
+++ frontend/src/actions/authentication.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setAuthToken from '../setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 export const registerUser = (user, history) => dispatch => {
   console.log(JSON.stringify(user));
@@ -21,17 +23,24 @@ export const registerUser = (user, history) => dispatch => {
 };
 
 export const loginUser = user => dispatch => {
-  console.log("we doin this");
   axios
     .post("/users/login", user)
     .then(res => {
-      console.log(res.data);
+      console.log(res);
+      const {token} = res;
+      localStorage.setItem('jwtToken',token);
+      setAuthToken(token);
+      const decoded= jwt_decode(token);
+      dispatch(setCurrentUser(decoded));
     })
     .catch(err => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      });
+      console.log("Could not log in");
     });
 };
 
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
