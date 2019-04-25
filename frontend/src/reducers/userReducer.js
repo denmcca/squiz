d0ab40e64defdd9f9432dafb36af5acbd4cf9c3b
Philip Poppler <philpoppler@gmail.com>
2019-04-25 11:39:59 -0700

    Revert to "Registration now working"

---
+++ frontend/src/reducers/userReducer.js
@@ -0,0 +1,28 @@
+import { UPDATE_EMAIL, UPDATE_PASSWORD }  from "../actions/types";
+
+const initialState = {
+    firstName: 'Test',
+    lastName: 'User', 
+    email: '',
+    password: '', 
+};
+
+const userReducer = (state = initialState, action) => {
+    const newState = {...state};
+    switch(action.type) {
+        case UPDATE_EMAIL:
+            return {
+                ...state,
+                email: action.value,
+            }
+        case UPDATE_PASSWORD:
+            return {
+                ...state,
+                password: action.value,
+            }
+        default:
+            return state;
+    }
+};
+
+export default userReducer;
